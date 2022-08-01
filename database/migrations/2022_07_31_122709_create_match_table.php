e8ec41547faab24104fbf423f1e4277b6459ea85
Fadilah Riczky <friczky@gmail.com>
2022-08-01 12:51:15 +0700

    :zap: add migration

---
+++ database/migrations/2022_07_31_122709_create_match_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMatchTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('match', function (Blueprint $table) {
+            $table->id();
+            $table->integer('clubs_id');
+            $table->integer('rivals_id');
+            $table->dateTime('schedule');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('match');
+    }
+}
---
+++ database/seeders/MatchTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class MatchTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
