5a4309313b0f3e4c1e7c24386fa75de7a787a519
Fadilah Riczky <friczky@gmail.com>
2022-08-02 17:48:00 +0700

    :fire: added models

--- app/Models/Managers.php
+++ app/Models/Managers.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Clubs extends Model
+class Managers extends Model
 {
     use HasFactory;
 }
